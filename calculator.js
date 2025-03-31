d17d04d602364f9abcdfbd20590190d76c2d8b7c
Wognod <sven.degen1998@gmail.com>
2025-03-31 09:33:30 +0200

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -8,19 +8,21 @@ let ingredient_count = a.getElementsByTagName("div").length;        // Zählt al
 
 for (let i = 1; i <= ingredient_count; i++) {                       //Wiederholt alles in der geschwungenen klammer so oft bi i <= ingredient_count ist
     element = document.getElementById("ingredient_" + i);           // Hole die Zutatenmänge aller 12 zutaten nacheinander 
-    object["x" + i] = element                                       // lege den wert von element im objekt "objekt" als x1 beim der ersten wiederholung, als x2 bei der zweiten, usw...
-    originalValues["x" + i] = parseFloat(element.innerText)         // speichert den ursprünglichen zahlenwert als float (zum rechnen) im objekt originalValues als x1 beim der ersten wiederholung, als x2 bei der zweiten, usw...
+    object["x" + i] = element;                                       // lege den wert von element im objekt "objekt" als x1 beim der ersten wiederholung, als x2 bei der zweiten, usw...
+    originalValues["x" + i] = parseFloat(element.innerText);         // speichert den ursprünglichen zahlenwert als float (zum rechnen) im objekt originalValues als x1 beim der ersten wiederholung, als x2 bei der zweiten, usw...
 }
+
+
 
 
 function claculate_ingredient() {                                   //führt funktion claculator für zutat 1-12 aus   diese funktion wird in html beim input als onChange ausgeführt
 
-
+    
 
     for (let i = 1; i <= ingredient_count; i++) {                   //
 
-
-        calculator(i)                                               //wird ausgeführt mit dem parameter 1 beim ersten mal 2 beim zweiten usw...
+        if(originalValues["x" + i] > 0) {
+        calculator(i) ;                   }                           //wird ausgeführt mit dem parameter 1 beim ersten mal 2 beim zweiten usw...
     }
 }
 
@@ -31,12 +33,7 @@ function calculator(y) {                                                    //ä
 
     let result = originalValues["x" + y] * Portion_number / 4;                  //rechnet das originalValue[x1-12] mal Portion_Number dur 4 da die orignalzutatenmäne schon für 4 portionen eingetragen wurden
     result = parseFloat(result.toFixed(2));                                 // Rundet das ergebniss auf 2 stellen hinter dem komma
-    console.log(result);    
-
     object["x" + y].innerText = result;                                     //ändert den "innerText" des jebweiligen Zutat object[x1], object[x2] usw...
-
-
-
     return result;
 }
 
